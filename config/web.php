7c6fa19869c1a91fe2aaed0c5e7d2c7eade82e27
LenaYu2016 <475099436yu@gmail.com>
2017-02-19 05:32:37 +0800

    thirdupdate

--- config/web.php
+++ config/web.php
@@ -9,7 +9,7 @@ $config = [
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => 'xdsD_0-Zc192bqICJ77pD8_86XXfeZZB',
+            'cookieValidationKey' => 'secret key',
             'parsers' => [
                 'application/json' => 'yii\web\JsonParser',
             ]
